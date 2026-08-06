72634025831bb812f6c26556a51bf8bdc317ea2f
PlayerNoob1 <sakamoto.vitor@gmail.com>
2026-08-05 22:26:07 -0300

    Add files via upload

---
+++ Aula2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	int a,b,c,d;
+	
+	a = 3;
+	b = 4;
+	c = 5;
+	d = (a+b+c)/3;
+	
+	printf("A media de %d, %d, %d = %d", a,b,c,d);
+
+	return 0;
+}
