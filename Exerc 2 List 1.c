5977aa2aec1013cc502892f9ec6baf656b8d71de
PlayerNoob1 <sakamoto.vitor@gmail.com>
2026-08-24 19:47:45 -0300

    Exercício 2 List 1

---
+++ Exerc 2 List 1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <MATH.H>
+
+	printf("=================EXERCICIO 2 SK=================\n");
+	
+	double valor, base;
+	int expoente;
+
+	printf("\nInsira um valor positivo: ");
+	scanf("%lf", &valor);//limitado a 6 casa decimais.
+
+	base = valor;
+	expoente = 0;
+
+	if (valor >= 10) {
+		valor /= 10;
+		expoente++;
+	}
+	if (valor >= 10) {
+		valor /= 10;
+		expoente++;
+	}
+	if (valor >= 10) {
+		valor /= 10;
+		expoente++;
+	}
+	if (valor >= 10) {
+		valor /= 10;
+		expoente++;
+	}
+	if (valor >= 10) {
+		valor /= 10;
+		expoente++;
+	}
+	if (valor >= 10) {
+		valor /= 10;
+		expoente++;
+	}
+	
+	printf("\nEm notacao cientifica: %.6f x 10^%d\n", base, expoente);
+
+return 0;
+}
