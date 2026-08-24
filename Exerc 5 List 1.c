294db9c8860d918fd39cf3aff1afe0cdf631ac46
PlayerNoob1 <sakamoto.vitor@gmail.com>
2026-08-24 20:06:14 -0300

    Exerc 5 List 1

---
+++ Exerc 5 List 1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <MATH.H>
+
+int main(int argc, char *argv[]) {
+
+
+	printf("=================EXERCICIO 5=================\n");
+
+	int m, s, p, valor1, valor2, valor3, valor4, v1, v2, v3, v4, i;
+
+	
+	printf("\nInforme um valor de 1 ate 6: ");
+	scanf("%d", &valor1);
+
+	printf("\nInforme outro valor : ");
+	scanf("%d", &valor2);
+
+	printf("\nInforme outro valor : ");
+	scanf("%d", &valor3);
+
+	printf("\nInforme outro valor : ");
+	scanf("%d", &valor4);
+
+	s = valor1 + valor2 + valor3 + valor4;
+	m = s / 4;
+	
+	v1 = valor1;
+	i = 0;
+	
+	if 
+	
+
+	printf("\nA soma dos valores é: %d", s);
+	printf("\nA media dos valores é: %d", m);
+	printf("\nO produtorio do primeiro valor e de %d", v1);
+
+return 0
+}
+	
