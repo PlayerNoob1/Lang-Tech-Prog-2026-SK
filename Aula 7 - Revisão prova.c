062506a2a5210c2a78fa1555b4ee2ffbaf2a24bd
Vitor Sakamoto <sakamoto.vitor@gmail.com>
2026-09-09 22:34:25 -0300

    Create Aula 7 - Revisão prova.c

---
+++ Aula 7 - Revisão prova.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Exercicio CPF - Link: https://profdaciomachado.github.io/html_didatc_resurces/listas/c/Lista.html
+int mult (int digito, int valor ){
+	digito * valor;
+}
+
+int main(int argc, char *argv[]) {
+	int dg1, dg2, dg3, dg4, dg5, dg6, dg7, dg8, dg9, dv1, dv2, soma, resto;
+	
+	scanf("%d %d %d . %d %d %d . %d %d %d - %d %d", 
+	     &dg1, &dg2, &dg3, &dg4, &dg5, &dg6, &dg7, &dg8, &dg9, &dv1, &dv2);
+	
+	printf("Confirme o CPF: %d%d%d.%d%d%d.d%d%d%-d%%d", 
+	     dg1, dg2, dg3, dg4, dg5, dg6, dg7, dg8, dg9, dv1, dv2);	  
+   
+// Primeira verificaação do CPF
+
+// Segunda verificação do CPF
+	
+	return 0;
+}
